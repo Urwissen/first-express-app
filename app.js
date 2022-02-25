bb97f87514bbf59eab240da7532b8665eea0a0dd
Lars <l.gessner@posteo.de>
2022-02-25 10:04:41 +0100

    add second route

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("I love coding!")
+    res.send("<h1>I love coding!</h1>")
 })
 
-app.listen(3000)
+app.get("/hello", (req, res) => {
+    res.send("<h1>Hello, JavaScript Developer!</h1>")
+})
+
+app.listen(3000, () => {
+    console.log("The application is running at localhost:3000")
+})
